0efab58659319ed1ba2885b812cf80072dded2a0
itsjh <93984898+itsjh1242@users.noreply.github.com>
2022-10-05 03:08:14 +0900

    1레벨 다풀었당

---
+++ ex01_20181510 - 복사본.js
@@ -0,0 +1,18 @@
+const myFunction = function(n, k){
+    var result = 0;
+    if(k == null){
+        k = 2;
+    };
+    if(n < 1 || k < 1){
+        return console.log("조건을 만족하지 않습니다.");
+    } else {
+        for(var i = 1; i <= n; i++){
+            if(i % k == 0){
+                result += i;
+            };
+        };
+    };
+    return console.log(result);
+};
+
+myFunction(100, 5);
